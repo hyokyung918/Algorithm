66e140910ce872c536e3b7c7c67e1461d02f6e62
SeongHyokyung <82697524+hyokyung918@users.noreply.github.com>
2023-10-15 15:31:26 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 156 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i=0; i<26; i++) {
+            arr[i] = -1;
+            for (int j=0; j<s.length(); j++) {
+                if (s.charAt(j) == (char)i+97 && arr[i] == -1)
+                    arr[i] = j;
+            }
+        }
+        for (int i=0; i<26; i++) {
+            System.out.printf(arr[i] + " ");
+        }
+    }
+}
