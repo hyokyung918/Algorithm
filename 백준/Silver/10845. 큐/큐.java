739f0197037820ef01879745f353aa1b7719048a
SeongHyokyung <82697524+hyokyung918@users.noreply.github.com>
2023-10-18 20:56:05 +0900

    [Silver IV] Title: 큐, Time: 400 ms, Memory: 21068 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -4,56 +4,50 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> q = new LinkedList();
         int n = Integer.parseInt(br.readLine());
-
-        int last = 0;
+        
+        int update = 0;
         for (int i=0; i<n; i++) {
             String s = br.readLine();
-            String[] order = s.split(" ");
-
-            switch (order[0]) {
-
+            String[] arr = s.split(" ");
+            
+            boolean isEmpty = q.isEmpty();
+            switch (arr[0]) {
+                case "push":
+                    int num =Integer.parseInt(arr[1]);
+                    q.offer(num);
+                    update = num;
+                    break;
+                case "pop":
+                    if (isEmpty) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(q.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(q.size());
+                    break;
                 case "empty":
-                    if (queue.isEmpty()) {
+                    if (isEmpty) {
                         System.out.println(1);
                     } else {
                         System.out.println(0);
                     }
                     break;
-
-                case "pop":
-                    if (queue.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(queue.poll());
-                    }
-                    break;
-
-                case "size":
-                    System.out.println(queue.size());
-                    break;
-
                 case "front":
-                    if (queue.isEmpty()) {
+                    if (isEmpty) {
                         System.out.println(-1);
                     } else {
-                        System.out.println(queue.peek());
+                        System.out.println(q.peek());
                     }
                     break;
-
-                case "push":
-                    int x = Integer.parseInt(order[1]);
-                    last = x;
-                    queue.offer(x);
-                    break;
-
                 case "back":
-                    if (queue.isEmpty()) {
+                    if (isEmpty) {
                         System.out.println(-1);
                     } else {
-                        System.out.println(last);
+                        System.out.println(update);
                     }
                     break;
             }
