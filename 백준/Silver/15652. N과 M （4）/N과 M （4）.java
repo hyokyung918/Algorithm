1a0c00d749b2e8e7102b70e30d156289c6c09719
SeongHyokyung <82697524+hyokyung918@users.noreply.github.com>
2023-11-14 19:41:32 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16516 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int now, int depth) {
+
+        if (depth == m) {
+            for (int e : arr) {
+                sb.append(e).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=now; i<=n; i++) {
+            arr[depth] = i;
+            dfs(i, depth+1);
+        }
+    }
+}
