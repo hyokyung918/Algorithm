206e593ba5b74529c6682dc133fc6d192081d30b
SeongHyokyung <82697524+hyokyung918@users.noreply.github.com>
2024-03-13 01:17:41 +0900

    [Silver III] Title: N과 M (8), Time: 172 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+    private static int n, m;
+    private static int[] arr, answer;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        answer = new int[m];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+    
+    private static void dfs(int cnt, int now) {
+        
+        if (cnt == m) {
+            
+            for (int e : answer) {
+                sb.append(e).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i=now; i<n; i++) {
+            
+            answer[cnt] = arr[i];
+            dfs(cnt+1, i);
+        }
+    }
+}
