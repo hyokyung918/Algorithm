3a9b0f20d0717a63752ab8928337dd77f8a717fc
SeongHyokyung <82697524+hyokyung918@users.noreply.github.com>
2024-03-14 01:13:42 +0900

    [Silver II] Title: N과 M (12), Time: 444 ms, Memory: 36716 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+    private static int n, m;
+    private static int[] input, output;
+    private static LinkedHashSet<String> set = new LinkedHashSet<>();
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        input = new int[n];
+        output = new int[m];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        
+        dfs(0, 0);
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+    
+    private static void dfs(int cnt, int current) {
+        
+        if (cnt == m) {
+            
+            String s = "";
+            for (int e : output) {
+                s += String.valueOf(e) + " ";
+            }
+            set.add(s);
+            return;
+        }
+        
+        for (int i=0; i<n; i++) {
+            
+            if (current > input[i])
+                continue;
+            
+            output[cnt] = input[i];
+            dfs(cnt+1, input[i]);
+        }
+    }
+}
